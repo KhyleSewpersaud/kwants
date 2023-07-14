4a0898fc0b98de50559ce8d5578bd491ca13dbb7
Khyle Sewpersaud <khylesewpersaud@gmail.com>
2023-07-13 20:31:16 -0400

    feat: Added two wishlist's and styled them for future use.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
+import './App.css'
+
 const App = () => {
-  return (
-    <div>
+  return ( 
+    <div className='site-container'>
+      <div className='header-container'>
+        <h1 className='header'>**kwants</h1>
+      </div>
+      
+
+      <div className='category'>
+        <h2 className='category-name'>Clothes</h2>
+        <div className='item-carousel'>
+          <div className='item'></div>
+          <div className='item'></div>
+          <div className='item'></div>
+        </div>
+      </div>
+
+      <div className='category'>
+        <h2 className='category-name'>Skincare</h2>
+        <div className='item-carousel'>
+          <div className='item'></div>
+          <div className='item'></div>
+          <div className='item'></div>
+        </div>
+      </div>
+
+      <button className=''>Add Wishlist</button>
+
     </div>
   );
 }
